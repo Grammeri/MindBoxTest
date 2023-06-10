770e3cf6c9eae474c46a82c74f31940c1c982d48
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-06-10 10:35:12 -0500

    App initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {TaskType, Todolist} from './Todolist';
+import {v1} from 'uuid';
+import {Box, Container, Grid, Paper, Typography} from '@mui/material';
+
+export type FilterValuesType = "all" | "active" | "completed";
+export type TasksStateType = {
+  [key: string]: Array<TaskType>
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let [tasks, setTasks] = useState<Array<TaskType>>([
+    {id: v1(), title: "Тестовое задание", isDone: true},
+    {id: v1(), title: "Прекрасный код", isDone: true},
+    {id: v1(), title: "Покрытие тестами", isDone: false},
+  ]);
+
+   return (
+      <div className="App">
+        <Container fixed>
+          <Grid container justifyContent="center" style={{padding: "20px 0px"}}>
+            <Paper style={{ padding: "20px", margin: "10px", backgroundColor: "lightgray", boxShadow: "none" }}>
+              <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100px">
+                <Typography variant="h4" component="h2" color="darkgray" gutterBottom>
+                  TODOS
+                </Typography>
+                <Typography variant="body2" component="p" gutterBottom>
+                  Тестовое задание Дмитрия Николаева
+                </Typography>
+              </Box>
+              <Paper style={{ padding: "10px", boxShadow: "none" }}>
+                <Todolist
+
+                />
+              </Paper>
+            </Paper>
+          </Grid>
+        </Container>
+      </div>
   );
 }
 
